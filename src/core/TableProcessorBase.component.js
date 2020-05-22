c55f6e8fd75950812057ec575d05a172db5c9481
barhan44 <gunyashov44@gmail.com>
2020-05-22 09:55:27 +0300

    Add: one little comment :)

--- src/core/TableProcessorBase.component.js
+++ src/core/TableProcessorBase.component.js
@@ -1,5 +1,6 @@
 import { DOMListener } from '@core/DOMListener';
 
+// Let's just imagine that this is an abstract class :)
 export class TableProcessorBaseComponent extends DOMListener {
   toHTML() {
     return '';
