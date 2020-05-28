b68c9ba95d956dded8fd73bcbdedf28d7425c9b3
sergey.gunyashov <gunyashov44@gmail.com>
2020-05-28 20:58:29 +0300

    Add: render table function

--- src/components/table/table.component.js
+++ src/components/table/table.component.js
@@ -1,33 +1,10 @@
 import { AbstractComponent } from '@core/AbstractComponent';
+import { createTable } from '@/components/table/table.template';
 
 export class TableComponent extends AbstractComponent {
   static className = 'table-processor__table';
 
   toHTML() {
-    return `<div class="row">
-                <div class="row-info"></div>
-                <div class="row-data">
-                    <div class="column">
-                        A
-                    </div>
-                    <div class="column">
-                        B
-                    </div>
-                    <div class="column">
-                        C
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="row-info">
-                    1
-                </div>
-                <div class="row-data">
-                    <div class="cell selected" contenteditable>A1</div>
-                    <div class="cell" contenteditable>B1</div>
-                    <div class="cell" contenteditable>C1</div>
-                </div>
-            </div>
-        </div>`;
+    return createTable();
   }
 }
