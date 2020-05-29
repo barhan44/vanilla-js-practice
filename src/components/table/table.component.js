f607ef3d3133a85c34b8b8fc2b921d30768b21a6
barhan44 <41652312+barhan44@users.noreply.github.com>
2020-05-30 01:02:56 +0300

    Add: table resizer (#4)

--- src/components/table/table.component.js
+++ src/components/table/table.component.js
@@ -1,10 +1,24 @@
 import { AbstractComponent } from '@core/AbstractComponent';
 import { createTable } from '@/components/table/table.template';
+import { resizeHandler } from '@/components/table/table.resize';
+import { shouldResize } from '@/components/table/table.helpers';
 
 export class TableComponent extends AbstractComponent {
   static className = 'table-processor__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable();
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
