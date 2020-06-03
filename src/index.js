af2cf403ff880969dae454f3da130c26640f53be
barhan44 <gunyashov44@gmail.com>
2020-06-03 22:01:47 +0300

    Add: store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './scss/index.scss';
 
+import { createStore } from '@core/utils/createStore.function';
 import { RootComponent } from '@/components/root/root.component';
 import { HeaderComponent } from '@/components/header/header.component';
 import { ToolbarComponent } from '@/components/toolbar/toolbar.component';
 import { FormulaComponent } from '@/components/formula/formula.component';
 import { TableComponent } from '@/components/table/table.component';
+import { rootReducer } from '@/store/rootReducer';
+
+const store = createStore(rootReducer);
 
 const tp = new RootComponent('#app', {
   components: [
@@ -13,6 +17,7 @@ const tp = new RootComponent('#app', {
     FormulaComponent,
     TableComponent,
   ],
+  store,
 });
 
 tp.render();
