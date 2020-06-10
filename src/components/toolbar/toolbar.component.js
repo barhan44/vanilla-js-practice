3cdb1d315198dc36dab9a00a7ee31274c1a28bbc
barhan44 <gunyashov44@gmail.com>
2020-06-10 09:48:50 +0300

    Add: state component

--- src/components/toolbar/toolbar.component.js
+++ src/components/toolbar/toolbar.component.js
@@ -1,22 +1,38 @@
-import { AbstractComponent } from '@core/AbstractComponent';
+import { StateComponent } from '@core/StateComponent';
 import { $ } from '@core/utils/dom.util';
 import { createToolbar } from '@/components/toolbar/toolbar.template';
 
-export class ToolbarComponent extends AbstractComponent {
+export class ToolbarComponent extends StateComponent {
   static className = 'table-processor__toolbar';
 
   constructor($root, options) {
     super($root, { name: 'Toolbar', listeners: ['click'], ...options });
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      textDecoration: 'none',
+      fontStyle: 'normal',
+    };
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return createToolbar();
+    return this.template;
   }
 
   onClick(event) {
     const $target = $(event.target);
     if ($target.data.type === 'button') {
-      console.log($target.data.value);
+      const value = JSON.parse($target.data.value);
+      const key = Object.keys(value)[0];
+      this.setState({ [key]: value[key] });
     }
   }
 }
