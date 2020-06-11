01bc5587e7d1104df50bfc41e4743e7d5bf60f1a
barhan44 <41652312+barhan44@users.noreply.github.com>
2020-06-11 15:14:03 +0300

    Tech/unit testing (#10)
    * Add: jest package
    
    * Add: createStore tests
    
    * Add: router tests

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-syntax-class-properties',
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
---
+++ src/core/router/Router.spec.js
@@ -0,0 +1,33 @@
+import { Router } from './Router';
+import { Page } from '../Page';
+
+class MockDashboardPage extends Page {
+  getRoot() {
+    const root = document.createElement('div');
+    root.innerHTML = 'dashboard';
+    return root;
+  }
+}
+class MockTableProcessorPage extends Page {}
+
+describe('Router:', () => {
+  let router;
+  let $root;
+
+  beforeEach(() => {
+    $root = document.createElement('div');
+    router = new Router($root, {
+      dashboard: MockDashboardPage,
+      tableProcessor: MockTableProcessorPage,
+    });
+  });
+
+  test('should be defined', () => {
+    expect(router).toBeDefined();
+  });
+
+  test('should render Dashboard Page', () => {
+    router.changePageHandler();
+    expect($root.innerHTML).toBe('<div>dashboard</div>');
+  });
+});
