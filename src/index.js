28d507eb89d7c1b9235aa29d38e300611a6071b6
barhan44 <41652312+barhan44@users.noreply.github.com>
2020-06-11 18:17:43 +0300

    Change: some refactoring and some improvements (#12)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/index.scss';
 import { Router } from '@core/router/Router';
 import { DashboardPage } from '@/pages/dashboard/Dashboard.page';
-import { TableProcessorPage } from '@/pages/TableProcessor.page';
+import { TableProcessorPage } from '@/pages/table-processor/TableProcessor.page';
 
 new Router('#app', {
   dashboard: DashboardPage,
